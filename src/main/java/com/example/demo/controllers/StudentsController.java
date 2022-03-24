6b6bef0138c9767a682c5dfb2ead915a157d4f36
gddeazevedo <gabrielcapuerj@gmail.com>
2022-03-24 14:11:21 -0300

    changing classes for records and fixing some configs

--- src/main/java/com/example/demo/controllers/StudentsController.java
+++ src/main/java/com/example/demo/controllers/StudentsController.java
@@ -2,24 +2,19 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Student;
 import com.example.demo.services.StudentsService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
-@AllArgsConstructor(onConstructor = @__(@Autowired))
-public class StudentsController {
-
-    private final StudentsService studentsService;
+public record StudentsController(StudentsService studentsService) {
 
     @GetMapping
     public List<Student> index(
             @RequestParam(
                 required = false,
-                    name = "orderBy"
+                name = "orderBy"
             ) Integer orderBy
     ) {
         System.out.println("orderBy: " + orderBy);
--- src/main/java/com/example/demo/services/StudentsService.java
+++ src/main/java/com/example/demo/services/StudentsService.java
@@ -3,9 +3,6 @@ package com.example.demo.services;
 import com.example.demo.models.Student;
 import com.example.demo.repositories.StudentsRepository;
 
-import lombok.AllArgsConstructor;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,10 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor(onConstructor = @__(@Autowired))
-public class StudentsService {
-
-    private final StudentsRepository repository;
+public record StudentsService(StudentsRepository repository) {
 
     public List<Student> index() {
         return repository.findAll();
